b4d47d982a09faf38f15f3228fe34e4382a652df
Reuben Wilson <reuben.h.wilson@gmail.com>
2016-07-28 15:27:46 -0500

    Server.js

---
+++ server.js
@@ -0,0 +1,53 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/hibiscus-test');
+
+
+var Prescription = require('models/prescription.js');
+
+
+
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8085;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware for all requests
+router.use(function(req, res, next){
+
+	console.log('Something is happening');
+	next();
+});
+
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('API listening on port: ' + port);
